0b5e6a2604359dce53e6676b13a8fbfebdddd1dd
Patrick Brünn <p.bruenn@beckhoff.com>
2015-03-25 14:22:16 +0100

    cleanup AmsHeader.h; fix some of the endianness issues in AmsHeader.h

--- AdsLib/AmsHeader.h
+++ AdsLib/AmsHeader.h
@@ -5,87 +5,85 @@
 #include "wrap_endian.h"
 
 #include "AdsDef.h"
+#include <array>
 #include <cstddef>
 
 #pragma pack (push, 1)
 struct AmsTcpHeader
 {
-	AmsTcpHeader(const uint32_t length = 0)
-	{
-		*((uint16_t*)(&buffer[0])) = 0;
-		*((uint32_t*)(&buffer[2])) = qToLittleEndian<uint32_t>(length);
-	}
-
 	AmsTcpHeader(const uint8_t *frame)
 	{
-		memcpy(buffer, frame, sizeof(buffer));
+		memcpy(this, frame, sizeof(*this));
 	}
+
+	AmsTcpHeader(const uint32_t numBytes = 0)
+		: reserved(0),
+		leLength(qToLittleEndian<uint32_t>(numBytes))
+	{}
 
 	uint32_t length() const
 	{
-		return qFromLittleEndian<uint32_t>(buffer + sizeof(uint16_t));
+		return qFromLittleEndian<uint32_t>((const uint8_t*)&leLength);
 	}
-
 private:
-	uint8_t buffer[sizeof(uint16_t) + sizeof(uint32_t)];
+	uint16_t reserved;
+	uint32_t leLength;
 };
 
 struct AoERequestHeader
 {
 	static const uint32_t SDO_UPLOAD = 0xF302;
 
-	uint32_t group;
-	uint32_t offset;
-	uint32_t length;
-
 	AoERequestHeader(uint16_t sdoIndex, uint8_t sdoSubIndex, uint32_t dataLength)
-		: group(SDO_UPLOAD),
-		offset(((uint32_t)sdoIndex) << 16 | sdoSubIndex),
-		length(dataLength)
-	{
-	}
+		: AoERequestHeader(SDO_UPLOAD, ((uint32_t)sdoIndex) << 16 | sdoSubIndex, dataLength)
+	{}
 
 	AoERequestHeader(uint32_t indexGroup, uint32_t indexOffset, uint32_t dataLength)
-		: group(indexGroup),
-		offset(indexOffset),
-		length(dataLength)
-	{
-	}
+		: leGroup(qToLittleEndian<uint32_t>(indexGroup)),
+		leOffset(qToLittleEndian<uint32_t>(indexOffset)),
+		leLength(qToLittleEndian<uint32_t>(dataLength))
+	{}
+
+private:
+	uint32_t leGroup;
+	uint32_t leOffset;
+	uint32_t leLength;
 };
 
 struct AdsWriteCtrlRequest
 {
-	uint16_t adsState;
-	uint16_t devState;
-	uint32_t length;
-
 	AdsWriteCtrlRequest(uint16_t ads, uint16_t dev, uint32_t dataLength)
-		: adsState(qToLittleEndian(ads)),
-		devState(qToLittleEndian(dev)),
-		length(qToLittleEndian(dataLength))
-	{
-	}
+		: leAdsState(qToLittleEndian<uint16_t>(ads)),
+		leDevState(qToLittleEndian<uint16_t>(dev)),
+		leLength(qToLittleEndian<uint32_t>(dataLength))
+	{}
+
+private:
+	uint16_t leAdsState;
+	uint16_t leDevState;
+	uint32_t leLength;
 };
 
 struct AdsAddDeviceNotificationRequest
 {
-	uint32_t group;
-	uint32_t offset;
-	uint32_t length;
-	uint32_t mode;
-	uint32_t maxDelay;
-	uint32_t cycleTime;
-	const uint8_t reserved[16];
-
 	AdsAddDeviceNotificationRequest(uint32_t __group, uint32_t __offset, uint32_t __length, uint32_t __mode, uint32_t __maxDelay, uint32_t __cycleTime)
-		: group(__group),
-		offset(__offset),
-		length(__length),
-		mode(__mode),
-		maxDelay(__maxDelay),
-		cycleTime(__cycleTime),
+		: leGroup(qToLittleEndian<uint32_t>(__group)),
+		leOffset(qToLittleEndian<uint32_t>(__offset)),
+		leLength(qToLittleEndian<uint32_t>(__length)),
+		leMode(qToLittleEndian<uint32_t>(__mode)),
+		leMaxDelay(qToLittleEndian<uint32_t>(__maxDelay)),
+		leCycleTime(qToLittleEndian<uint32_t>(__cycleTime)),
 		reserved()
 	{}
+
+private:
+	uint32_t leGroup;
+	uint32_t leOffset;
+	uint32_t leLength;
+	uint32_t leMode;
+	uint32_t leMaxDelay;
+	uint32_t leCycleTime;
+	const std::array<uint8_t, 16> reserved;
 };
 
 using AdsDelDeviceNotificationRequest = uint32_t;
@@ -106,6 +104,8 @@ struct AoEHeader
 	static const uint16_t DEVICE_NOTIFICATION = 0x0008;
 	static const uint16_t READ_WRITE = 0x0009;
 
+
+	//TODO fix endianess!!!
     AmsAddr targetAddr;
     AmsAddr sourceAddr;
     const uint16_t cmdId;
@@ -154,7 +154,8 @@ using AoEWriteResponseHeader = uint32_t;
 
 struct AoEResponseHeader
 {
-	uint32_t result;
+	const uint32_t result;
+
 	AoEResponseHeader()
 		: result(0)
 	{}
@@ -162,11 +163,12 @@ struct AoEResponseHeader
 	AoEResponseHeader(const uint8_t *frame)
 		: result(qFromLittleEndian<uint32_t>(frame))
 	{}
+
 };
 
 struct AoEAddNotificationResponseHeader : AoEResponseHeader
 {
-	uint32_t hNotify;
+	const uint32_t hNotify;
 	AoEAddNotificationResponseHeader()
 		: hNotify(0)
 	{}
@@ -177,19 +179,17 @@ struct AoEAddNotificationResponseHeader : AoEResponseHeader
 	{}
 };
 
-struct AoEReadResponseHeader
+struct AoEReadResponseHeader : AoEResponseHeader
 {
-    const uint32_t result;
     const uint32_t readLength;
 
 	AoEReadResponseHeader()
-		: result(0),
-		readLength(0)
+		: readLength(0)
 	{}
 
     AoEReadResponseHeader(const uint8_t *frame)
-        : result(qFromLittleEndian<uint32_t>(frame)),
-          readLength(qFromLittleEndian<uint32_t>(frame + sizeof(result)))
+        : AoEResponseHeader(frame),
+		readLength(qFromLittleEndian<uint32_t>(frame + sizeof(AoEResponseHeader)))
 	{}
 };
 #pragma pack (pop)
