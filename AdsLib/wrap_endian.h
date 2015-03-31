5df1a09a7ff4401c092973461866c2082c5d2ffd
Patrick Brünn <p.bruenn@beckhoff.com>
2015-03-31 11:26:11 +0200

    make qFromLittleEndian() a NOP on little endian; remove marker from big endian tree

--- AdsLib/wrap_endian.h
+++ AdsLib/wrap_endian.h
@@ -5,7 +5,6 @@
 
 // WARNING code was never tested on a big endian machine!
 #if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
-blub
 template<class T>
 inline T qToLittleEndian(const T value)
 {
@@ -35,11 +34,7 @@ inline T qToLittleEndian(const T value)
 template<class T>
 inline T qFromLittleEndian(const uint8_t *value)
 {
-    T result = 0;
-    for (int i = 0; i < sizeof(T); ++i) {
-        result += (value[i] << (8 * i));
-    }
-    return result;
+    return *reinterpret_cast<const T*>(value);
 }
 
 inline uint16_t qToBigEndian(const uint16_t value)
