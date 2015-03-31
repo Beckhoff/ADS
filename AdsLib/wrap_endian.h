a554746754f05224c6407da4efe258bf7a50beed
Patrick Brünn <p.bruenn@beckhoff.com>
2015-03-31 10:42:07 +0200

    fix endianess macros for big endian

--- AdsLib/wrap_endian.h
+++ AdsLib/wrap_endian.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 // WARNING code was never tested on a big endian machine!
-#if defined(_WIN32) || defined(__ORDER_LITTLE_ENDIAN__)
+#if defined(_WIN32) || (BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
 template<class T>
 inline T qToLittleEndian(const T value)
 {
@@ -25,5 +25,27 @@ inline uint16_t qToBigEndian(const uint16_t value)
 {
     return ((value & 0xff) << 8) | ((value & 0xff00) >> 8);
 }
+#else
+template<class T>
+inline T qToLittleEndian(const T value)
+{
+	return ((value & 0xff) << 8) | ((value & 0xff00) >> 8);
+}
+
+template<class T>
+inline T qFromLittleEndian(const uint8_t *value)
+{
+	T result = 0;
+	for (int i = 0; i < sizeof(T); ++i) {
+		result += (value[i] << (8 * (sizeof(T) - 1 - i));
+	}
+	return result;
+}
+
+inline uint16_t qToBigEndian(const uint16_t value)
+{
+	return value;
+}
+
 #endif
 #endif // QENDIAN_H
