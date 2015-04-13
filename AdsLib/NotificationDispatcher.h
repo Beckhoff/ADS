6af8b7728d56e4d4cd192f5ac9e10e0bcaace465
Patrick Brünn <p.bruenn@beckhoff.com>
2015-04-13 17:25:23 +0200

    refactor NotificationId

--- AdsLib/NotificationDispatcher.h
+++ AdsLib/NotificationDispatcher.h
@@ -9,7 +9,7 @@
 #include <map>
 #include <thread>
 
-struct NotificationDispatcher;
+struct NotificationId;
 
 struct AmsProxy
 {
@@ -35,84 +35,15 @@ struct VirtualConnection
 	}
 };
 
-struct NotificationId
-{
-	NotificationId(uint32_t __hNotify, std::shared_ptr<NotificationDispatcher> __dispatcher)
-		: hNotify(__hNotify),
-		dispatcher(__dispatcher)
-	{}
-
-	bool operator <(const NotificationId &ref) const
-	{
-		return hNotify < ref.hNotify;
-	}
-
-	const uint32_t hNotify;
-	const std::shared_ptr<NotificationDispatcher> dispatcher;
-};
-
 struct NotificationDispatcher
 {
-	NotificationDispatcher(AmsProxy &__proxy, AmsAddr __amsAddr, uint16_t __port)
-		: ring(4 * 1024 * 1024),
-		amsAddr(__amsAddr),
-		port(__port),
-		thread(&NotificationDispatcher::Run, this),
-		proxy(__proxy)
-	{}
+	NotificationDispatcher(AmsProxy &__proxy, AmsAddr __amsAddr, uint16_t __port);
 
-	~NotificationDispatcher()
-	{
-		sem.Stop();
-		thread.join();
-	}
-
-	NotificationId Emplace(PAdsNotificationFuncEx pFunc, uint32_t hUser, uint32_t length, uint32_t hNotify, std::shared_ptr<NotificationDispatcher> dispatcher)
-	{
-		std::lock_guard<std::recursive_mutex> lock(mutex);
-		notifications.emplace(hNotify, Notification{ pFunc, hNotify, hUser, length, amsAddr, port });
-		return NotificationId{ hNotify, dispatcher };
-	}
-
-	bool Erase(uint32_t hNotify, uint32_t tmms)
-	{
-		const auto status = proxy.__DeleteNotification(amsAddr, hNotify, tmms, port);
-		std::lock_guard<std::recursive_mutex> lock(mutex);
-		return !!notifications.erase(hNotify) && status;
-	}
-
+	~NotificationDispatcher();
+	NotificationId Emplace(PAdsNotificationFuncEx pFunc, uint32_t hUser, uint32_t length, uint32_t hNotify, std::shared_ptr<NotificationDispatcher> dispatcher);
+	bool Erase(uint32_t hNotify, uint32_t tmms);
 	inline void Notify() { sem.Post(); }
-
-	void Run()
-	{
-		while (sem.Wait()) {
-			const auto length = ring.ReadFromLittleEndian<uint32_t>();
-			(void)length;
-			const auto numStamps = ring.ReadFromLittleEndian<uint32_t>();
-			for (uint32_t stamp = 0; stamp < numStamps; ++stamp) {
-				const auto timestamp = ring.ReadFromLittleEndian<uint64_t>();
-				const auto numSamples = ring.ReadFromLittleEndian<uint32_t>();
-				for (uint32_t sample = 0; sample < numSamples; ++sample) {
-					const auto hNotify = ring.ReadFromLittleEndian<uint32_t>();
-					const auto size = ring.ReadFromLittleEndian<uint32_t>();
-					std::lock_guard<std::recursive_mutex> lock(mutex);
-					auto it = notifications.find(hNotify);
-					if (it != notifications.end()) {
-						auto &notification = it->second;
-						if (size != notification.Size()) {
-							LOG_WARN("Notification sample size: " << size << " doesn't match: " << notification.Size());
-							ring.Read(size);
-							return;
-						}
-						notification.Notify(timestamp, ring);
-					}
-					else {
-						ring.Read(size);
-					}
-				}
-			}
-		}
-	}
+	void Run();
 
 	RingBuffer ring;
 	const AmsAddr amsAddr;
@@ -123,5 +54,24 @@ private:
 	Semaphore sem;
 	std::thread thread;
 	AmsProxy &proxy;
+};
+
+struct NotificationId
+{
+	NotificationId(uint32_t __hNotify, std::shared_ptr<NotificationDispatcher> __dispatcher)
+		: hNotify(__hNotify),
+		dispatcher(__dispatcher)
+	{}
+
+	bool operator <(const NotificationId &ref) const
+	{
+		if (hNotify != ref.hNotify) {
+			return hNotify < ref.hNotify;
+		}
+		return dispatcher->amsAddr < ref.dispatcher->amsAddr;
+	}
+
+	const uint32_t hNotify;
+	const std::shared_ptr<NotificationDispatcher> dispatcher;
 };
 #endif /* #ifndef _NOTIFICATION_DISPATCHER_H_ */
