5a452350d9c0e95a4f0eeb9eeff493611481dd28
oscar sanhueza <oscar.sanhueza@gmail.com>
2016-05-13 17:33:28 +0300

    Add notification to variable created by handle
    Is a bit difficult to figure out how to use the handle
    created by name in order to get notifications.
    
    I think is positive to add it in the example
    
    Signed-off-by: oscar sanhueza <oscar.sanhueza@gmail.com>

--- example/example.cpp
+++ example/example.cpp
@@ -122,6 +122,62 @@ void readStateExample(std::ostream& out, long port, const AmsAddr& server)
     out << "ADS state: " << std::dec << adsState << " devState: " << std::dec << devState << '\n';
 }
 
+void notificationByNameExample(std::ostream& out, long port, const AmsAddr& server)
+{
+    const AdsNotificationAttrib attrib = {
+        1,
+        ADSTRANS_SERVERCYCLE,
+        0,
+        {4000000}
+    };
+    uint32_t hNotify;
+    uint32_t hUser = 0;
+
+
+    uint32_t bytesRead;
+    char handleName[] = "MAIN.byByte";
+    uint32_t handle;
+
+    out << __FUNCTION__ << "():\n";
+    const long handleStatus = AdsSyncReadWriteReqEx2(port,
+                                                     &server,
+                                                     ADSIGRP_SYM_HNDBYNAME,
+                                                     0,
+                                                     sizeof(handle),
+                                                     &handle,
+                                                     sizeof(handleName),
+                                                     handleName,
+                                                     &bytesRead);
+
+     if (handleStatus) {
+         out << "Create handle for '" << handleName << "' failed with: 0x" << std::hex << handleStatus << '\n';
+         return;
+     }
+
+    const long addStatus = AdsSyncAddDeviceNotificationReqEx(port,
+                                                             &server,
+                                                             ADSIGRP_SYM_VALBYHND,
+                                                             handle,
+                                                             &attrib,
+                                                             &NotifyCallback,
+                                                             hUser,
+                                                             &hNotify);
+    if (addStatus) {
+        out << "Add device notification failed with: " << std::dec << addStatus << '\n';
+        return;
+    }
+
+    std::cout << "Hit ENTER to stop by name notifications\n";
+    std::cin.ignore();
+
+    const long delStatus = AdsSyncDelDeviceNotificationReqEx(port, &server, hNotify);
+    if (delStatus) {
+        out << "Delete device notification failed with: " << std::dec << delStatus;
+        return;
+    }
+}
+
+
 void runExample(std::ostream& out)
 {
     static const AmsNetId remoteNetId { 192, 168, 0, 231, 1, 1 };
@@ -142,6 +198,7 @@ void runExample(std::ostream& out)
 
     const AmsAddr remote { remoteNetId, AMSPORT_R0_PLC_TC3 };
     notificationExample(out, port, remote);
+    notificationByNameExample(out, port, remote);
     readExample(out, port, remote);
     readByNameExample(out, port, remote);
     readStateExample(out, port, remote);
