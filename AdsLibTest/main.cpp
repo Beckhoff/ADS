97f890fd3f8691ac2d6e5bb354e5575a267c68fc
Patrick Bruenn <p.bruenn@beckhoff.com>
2017-05-29 07:44:07 +0200

    AdsLibTest: fix annoying performance warning (implicit int to bool)

--- AdsLibTest/main.cpp
+++ AdsLibTest/main.cpp
@@ -97,26 +97,26 @@ struct TestAmsRouter : test_base<TestAmsRouter> {
 
         // test new Ams with new Ip
         fructose_assert(0 == testee.AddRoute(netId_1, ip_local));
-        fructose_assert(testee.GetConnection(netId_1));
+        fructose_assert(!!testee.GetConnection(netId_1));
         fructose_assert(ip_local == testee.GetConnection(netId_1)->destIp);
 
         // existent Ams with new Ip -> close old connection manually, now
         fructose_assert(ROUTERERR_PORTALREADYINUSE == testee.AddRoute(netId_1, ip_remote));
-        fructose_assert(testee.GetConnection(netId_1));
+        fructose_assert(!!testee.GetConnection(netId_1));
         fructose_assert(ip_local == testee.GetConnection(netId_1)->destIp);
         testee.DelRoute(netId_1);
         fructose_assert(0 == testee.AddRoute(netId_1, ip_remote));
-        fructose_assert(testee.GetConnection(netId_1));
+        fructose_assert(!!testee.GetConnection(netId_1));
         fructose_assert(ip_remote == testee.GetConnection(netId_1)->destIp);
 
         // new Ams with existent Ip
         fructose_assert(0 == testee.AddRoute(netId_2, ip_remote));
-        fructose_assert(testee.GetConnection(netId_2));
+        fructose_assert(!!testee.GetConnection(netId_2));
         fructose_assert(ip_remote == testee.GetConnection(netId_2)->destIp);
 
         // already exists
         fructose_assert(0 == testee.AddRoute(netId_1, ip_remote));
-        fructose_assert(testee.GetConnection(netId_1));
+        fructose_assert(!!testee.GetConnection(netId_1));
         fructose_assert(ip_remote == testee.GetConnection(netId_1)->destIp);
     }
 
@@ -130,21 +130,21 @@ struct TestAmsRouter : test_base<TestAmsRouter> {
 
         // add + remove -> null
         fructose_assert(0 == testee.AddRoute(netId_1, ip_remote));
-        fructose_assert(testee.GetConnection(netId_1));
+        fructose_assert(!!testee.GetConnection(netId_1));
         fructose_assert(ip_remote == testee.GetConnection(netId_1)->destIp);
         testee.DelRoute(netId_1);
         fructose_assert(!testee.GetConnection(netId_1));
 
         // add_1, add_2, remove_1 -> null_1 valid_2
         fructose_assert(0 == testee.AddRoute(netId_1, ip_remote));
-        fructose_assert(testee.GetConnection(netId_1));
+        fructose_assert(!!testee.GetConnection(netId_1));
         fructose_assert(ip_remote == testee.GetConnection(netId_1)->destIp);
         fructose_assert(0 == testee.AddRoute(netId_2, ip_local));
-        fructose_assert(testee.GetConnection(netId_2));
+        fructose_assert(!!testee.GetConnection(netId_2));
         fructose_assert(ip_local == testee.GetConnection(netId_2)->destIp);
         testee.DelRoute(netId_1);
         fructose_assert(!testee.GetConnection(netId_1));
-        fructose_assert(testee.GetConnection(netId_2));
+        fructose_assert(!!testee.GetConnection(netId_2));
     }
 
     void testAmsRouterSetLocalAddress(const std::string&)
@@ -184,7 +184,7 @@ private:
         for (uint8_t i = 0; i < 255; ++i) {
             AmsNetId netId {192, 168, 0, i, 0, id};
             fructose_assert_eq(0, testee.AddRoute(netId, ip));
-            fructose_assert(testee.GetConnection(netId));
+            fructose_assert(!!testee.GetConnection(netId));
             fructose_assert(ip == testee.GetConnection(netId)->destIp);
         }
 
