cdf679f22f13d34518c57fcb49304b8a786983d7
Patrick Bruenn <p.bruenn@beckhoff.com>
2018-01-09 06:40:02 +0100

    tools: fructose: use version 1.4.0

--- tools/10_get_fructose.sh
+++ tools/10_get_fructose.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-NAME=fructose-1.3.0
+NAME=fructose-1.4.0
+FILENAME=${NAME}.tar.gz
 
 pushd ${SCRIPT_PATH}
 
 if ! sha512sum -c ${NAME}.sha512; then
-	wget https://downloads.sourceforge.net/project/fructose/fructose/${NAME}/${NAME}.tgz
+	wget https://downloads.sourceforge.net/project/fructose/fructose/${NAME}/${FILENAME}
 fi
 
-tar xf ${NAME}.tgz
+tar xf ${FILENAME}
 mv ${NAME}/fructose/include/fructose/ ${SCRIPT_PATH}/
 rm -rf ${NAME}/
-
-patch fructose/test_root.h 0001-fructose-test_root-split-use-size_t-for-start-end.patch
-patch fructose/test_root.h 0002-fructose-use-int-for-i-iterating-over-argc.patch
