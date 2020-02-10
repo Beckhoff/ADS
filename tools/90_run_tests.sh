1cd937a4426bcbaeb3cbc871b4d374e9581097cf
Patrick Bruenn <p.bruenn@beckhoff.com>
2020-02-10 13:43:22 +0100

    ci: 90_run_test was horribly broken

--- tools/90_run_tests.sh
+++ tools/90_run_tests.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+set -e
+set -u
+
+cleanup() {
+	${ncat_pid+kill ${ncat_pid}}
+}
+trap cleanup EXIT INT TERM
+
 # setup fake ads server and install cleanup trap
 ncat -l 48898 --keep-open &
 ncat_pid=$!
-trap "{ kill ${ncat_pid}; exit; }" EXIT
-
-set -e
 
 # wait for fake ads server to accept connections
 while ! ncat --send-only localhost 48898; do sleep 1; done
