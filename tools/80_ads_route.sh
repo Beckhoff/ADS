e93f9a892eb4857217469e54645d1d46730bc409
Patrick Bruenn <p.bruenn@beckhoff.com>
2021-07-21 14:59:59 +0200

    tools: code format 80_ads_route.sh
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- tools/80_ads_route.sh
+++ tools/80_ads_route.sh
@@ -4,23 +4,23 @@
 # Author: Patrick Bruenn <p.bruenn@beckhoff.com>
 
 add_route_ads() {
-        local _container_ip
-        _container_ip="$(ip_route_src "${1}")"
-        echo "CONTAINER_IP: ${_container_ip}"
-        while ! ${script_path}/add-route.py --route_name "Testroute" --sender_ams "${_container_ip}.1.1" --route_dest "${_container_ip}" --plc_username Administrator --plc_password 1 --plc_ip "${test_device}" ; do
-                echo "Adding ads route ..."
-                sleep 1
-        done
+	local _container_ip
+	_container_ip="$(ip_route_src "${1}")"
+	echo "CONTAINER_IP: ${_container_ip}"
+	while ! ${script_path}/add-route.py --route_name "Testroute" --sender_ams "${_container_ip}.1.1" --route_dest "${_container_ip}" --plc_username Administrator --plc_password 1 --plc_ip "${test_device}"; do
+		echo "Adding ads route ..."
+		sleep 1
+	done
 }
 
 host_as_ip() {
-        local _host="${1}"
-        {
-                # getent hosts will fail for ip or bad hostnames. In case _host was already
-                # an ip address we simply print it again. This would pass back a bad
-                # hostname, too, but we can't easily distingish here so let us fail later.
-                getent hosts "${_host}" || printf '%s' "${_host}"
-        } | cut -f1 -d" "
+	local _host="${1}"
+	{
+		# getent hosts will fail for ip or bad hostnames. In case _host was already
+		# an ip address we simply print it again. This would pass back a bad
+		# hostname, too, but we can't easily distingish here so let us fail later.
+		getent hosts "${_host}" || printf '%s' "${_host}"
+	} | cut -f1 -d" "
 }
 
 install_pyads() {
