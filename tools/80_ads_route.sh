be819bebcb48a202bef97f9b47f00b3885660793
Patrick Bruenn <p.bruenn@beckhoff.com>
2022-02-02 12:17:41 +0100

    ci: use new ip_route_src, with hostname support
    ip_route_src gained hostname support, which allows us to drop our own
    implementation.
    We upgrade the test_runner container, to the latest shlib from bullseye
    repos. There was a bug in 4.3, which caused us to install buster
    versions of Beckhoff debian tools.
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- tools/80_ads_route.sh
+++ tools/80_ads_route.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 # SPDX-License-Identifier: MIT
-# Copyright (C) 2021 Beckhoff Automation GmbH & Co. KG
+# Copyright (C) 2021 - 2022 Beckhoff Automation GmbH & Co. KG
 # Author: Patrick Bruenn <p.bruenn@beckhoff.com>
-
-my_ip() {
-	local _dest_ip="${1}"
-	if ! command -v ip_route_src > /dev/null; then
-		ifconfig "$(route get "${_dest_ip}" | grep "interface:" | cut -d' ' -f4)" inet | tr "[:blank:]" "\n" | awk '/inet|src/{getline; print}'
-	else
-		ip_route_src "${_dest_ip}"
-	fi
-}
 
 add_route_ads() {
 	local _container_ip
-	_container_ip="$(my_ip "${1}")"
+	_container_ip="$(ip_route_src "${1}")"
 	echo "CONTAINER_IP: ${_container_ip}"
 
 	while ! ./build/adstool "${test_device}" addroute "--netid=${_container_ip}.1.1" "--addr=${BHF_CI_NAT_IP-${_container_ip}}" --password=1; do
@@ -23,20 +14,10 @@ add_route_ads() {
 	done
 }
 
-host_as_ip() {
-	local _host="${1}"
-	{
-		# getent hosts will fail for ip or bad hostnames. In case _host was already
-		# an ip address we simply print it again. This would pass back a bad
-		# hostname, too, but we can't easily distingish here so let us fail later.
-		getent hosts "${_host}" || printf '%s' "${_host}"
-	} | cut -f1 -d" "
-}
-
 set -e
 set -u
 
 readonly script_path="$(cd "$(dirname "${0}")" && pwd)"
 readonly test_device=ads-server
 
-add_route_ads "$(host_as_ip "${test_device}")"
+add_route_ads "${test_device}"
