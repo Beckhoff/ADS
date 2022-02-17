ef3eb7d320e664f90744504d0322d380887b875d
Patrick Bruenn <p.bruenn@beckhoff.com>
2022-02-17 11:33:32 +0100

    ci: test variable TCP port
    We claim we can connect to TwinCAT routers listening on different ports
    than 48898, lets proof it by creating a socat port forwarding and
    connecting to the local socat port.
    At the moment socat is not easily available for TC/BSD as soon as that
    changes, we will expand our test to TC/BSD, too. Windows might be more
    difficult, we care about that later, when we care about Windows again.
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- tools/90_run_tests.sh
+++ tools/90_run_tests.sh
@@ -6,6 +6,7 @@ set -u
 cleanup() {
 	set +e
 	${ncat_pid+kill ${ncat_pid}}
+	${socat_pid+kill ${socat_pid}}
 }
 trap cleanup EXIT INT TERM
 
@@ -13,6 +14,19 @@ readonly script_path="$(cd "$(dirname "$0")" && pwd)"
 
 "${script_path}/80_ads_route.sh"
 "${script_path}/91_test_AdsTool.sh"
+
+# Setup socat port forwarding to test variable ADS ports. We have no socat on
+# Windows and not yet on TC/BSD, but we want to make sure we test on Linux,
+# where socat should be available!
+if test "Linux" = "$(uname)"; then
+	socat TCP-LISTEN:12345,fork TCP:ads-server:48898 &
+	socat_pid=$!
+	while ! nc -z localhost 12345; do
+		sleep 1
+		echo waiting for socat
+	done
+	./build/adstool "$(./build/adstool ads-server netid)" --gw=127.0.0.1:12345 --localams="$(ip_route_src ads-server).1.1" license systemid
+fi
 
 # setup fake ads server and install cleanup trap
 nc -l 48898 -k &
