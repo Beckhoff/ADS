2afafc520e3092252db1bc80a7d38e393e8d630d
Patrick Bruenn <p.bruenn@beckhoff.com>
2022-08-24 14:45:15 +0200

    ci: prepare a test-stage test for AdsLib
    We want to run AdsLib tests on a TC/BSD target. For this we can easily
    reuse our test-stage tool, which creates a QEMU VM within our generic
    test-runner container and installs TC/BSD into this VM.
    With this we cann then simply copy our build artifacts and test scripts
    into the VM and run our tests there just like we already do within our
    Linux containers.
    One difference is routing, but we already have a mechanisim for NAT
    routing, which we can reuse here to allow the QEMU VM to use the NAT of
    the test-runner container to communicat ADS to the docker service
    container, which contains our test TwinCAT endpoint.
    
    NOTE: The vim package on TC/BSD provides xxd, which is required by the
    tests.
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

---
+++ .test-stage/tests/run_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Beckhoff Automation GmbH & Co. KG
+
+set -e
+set -u
+
+script_path="$(cd "$(dirname "$0")" && pwd)"
+. "${script_path}/../lib_ssh_test.sh"
+
+# Copy all necessary files from test-runner container into the test-device.
+scp -r \
+	"${script_path}/../../build" \
+	"${script_path}/../../debian" \
+	"${script_path}/../../example" \
+	"${script_path}/../../tools" \
+	"Administrator@${test_device}:"
+
+# Run our generic tests within the QEMU test-device.
+${ssh_cmd} /bin/sh -$- <<- EOF
+	export BHF_CI_NAT_IP="$(ip_route_src ads-server)"
+	export EXPECTED_PLATFORMID="${EXPECTED_PLATFORMID}"
+	export EXPECTED_SYSTEMID="${EXPECTED_SYSTEMID}"
+	./tools/90_run_tests.sh
+EOF
