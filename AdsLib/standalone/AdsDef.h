958986c0db58d31f7b3004f321100d96ffdf311e
Patrick Bruenn <p.bruenn@beckhoff.com>
2022-08-25 22:07:35 +0200

    AdsLib: convert AMSPORT defines to enum
    MSVC complains when we pass our old school defines to various ADS
    functions, which expect a uint16_t value. Well, even if MSVC should know
    that these values fit perfectly, we can use this opportunity to make our
    declaration a bit nicer and finally use a typed enum for our AMSPORT
    constants.
    NOTE: MSVC will still complain about these defines, when compiling
    against TwinCATs native TcAdsDll.dll.
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- AdsLib/standalone/AdsDef.h
+++ AdsLib/standalone/AdsDef.h
@@ -12,20 +12,22 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 // AMS Ports
-#define AMSPORT_LOGGER                  100
-#define AMSPORT_R0_RTIME                200
-#define AMSPORT_R0_TRACE                (AMSPORT_R0_RTIME + 90)
-#define AMSPORT_R0_IO                   300
-#define AMSPORT_R0_SPS                  400
-#define AMSPORT_R0_NC                   500
-#define AMSPORT_R0_ISG                  550
-#define AMSPORT_R0_PCS                  600
-#define AMSPORT_R0_PLC                  801
-#define AMSPORT_R0_PLC_RTS1             801
-#define AMSPORT_R0_PLC_RTS2             811
-#define AMSPORT_R0_PLC_RTS3             821
-#define AMSPORT_R0_PLC_RTS4             831
-#define AMSPORT_R0_PLC_TC3              851
+enum AMSPORT : uint16_t {
+    AMSPORT_LOGGER = 100,
+    AMSPORT_R0_RTIME = 200,
+    AMSPORT_R0_TRACE = (AMSPORT_R0_RTIME + 90),
+    AMSPORT_R0_IO = 300,
+    AMSPORT_R0_SPS = 400,
+    AMSPORT_R0_NC = 500,
+    AMSPORT_R0_ISG = 550,
+    AMSPORT_R0_PCS = 600,
+    AMSPORT_R0_PLC = 801,
+    AMSPORT_R0_PLC_RTS1 = 801,
+    AMSPORT_R0_PLC_RTS2 = 811,
+    AMSPORT_R0_PLC_RTS3 = 821,
+    AMSPORT_R0_PLC_RTS4 = 831,
+    AMSPORT_R0_PLC_TC3 = 851
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 // ADS Cmd Ids
