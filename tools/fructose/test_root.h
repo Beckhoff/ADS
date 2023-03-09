00f6b8149ea3d365ae5ae83d0e1e729adc93ee11
Jan Ole Hüser <J.Hueser@beckhoff.com>
2023-03-09 16:49:46 +0100

    fructose: include <limits> for std::numeric_limits
    Signed-off-by: Jan Ole Hüser <J.Hueser@beckhoff.com>
    Reviewed-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- tools/fructose/test_root.h
+++ tools/fructose/test_root.h
@@ -44,6 +44,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <algorithm>
 #include <stdexcept>
 #include <sstream>
+#include <limits>
 
 #include <cstdlib>
 
