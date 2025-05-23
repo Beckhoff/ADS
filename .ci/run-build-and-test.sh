766abac805370ccc337331b689b8ed790b0873f7
Patrick Bruenn <p.bruenn@beckhoff.com>
2025-05-23 12:49:44 +0200

    ci: merge alpine, arch and mxe into a parallel job
    For these targets we run no tests, so we don't have to specify complex
    needs/dependencies. So we merge them into a parallel:matrix job.
    
    We move the flavor specific preparation steps into
    .ci/run-build-and-test.sh, because the matrix would become unreadable.
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- .ci/run-build-and-test.sh
+++ .ci/run-build-and-test.sh
@@ -5,6 +5,21 @@
 set -e
 set -u
 
+# Some flavor specific customizations
+case "${CI_JOB_NAME}" in
+	*alpine*)
+		apk add g++ meson
+		;;
+	*arch*)
+		pacman --noconfirm --refresh --sync --sysupgrade clang meson
+		;;
+	*mxe*)
+		BHF_CI_MESON_OPTIONS='--cross-file meson.cross.amd64-linux.win32'
+		;;
+	*)
+		;;
+esac
+
 meson setup build ${BHF_CI_MESON_OPTIONS-}
 ninja -C build
 meson setup example/build example ${BHF_CI_MESON_OPTIONS-}
