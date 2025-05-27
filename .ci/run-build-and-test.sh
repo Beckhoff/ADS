987d376368e460a089f722b4502a31eae68901f5
Patrick Bruenn <p.bruenn@beckhoff.com>
2025-05-27 13:46:06 +0200

    ci: merge test and build jobs
    Finally, we are able to merge the scatter build and test jobs into a
    single job. This drastically reduces the complexity of our
    .gitlab-ci.yml, because we don't need to propagate build artifacts into
    test jobs anymore.
    
    For this to work all we have to do us use more of our platform dependend
    configuration into .ci/run-build-and-test.sh and move the call of
    "./tools/90_run_tests.sh", too.
    
    Note we make heavy use of the CI_JOB_NAME to match these flavor specific
    settings. GitLab will add the parallel:matrix values into the
    CI_JOB_NAME, which allows us to use POSIX shell case matching to
    distingush between flavors. We use this to disable tests for
    "build-only" jobs, too.
    
    Our native linux-gcc jobs are now named "tclur" to better match the
    meson.native.tclur file.
    
    NOTE: We don't merge the FreeBSD jobs just yet. They will follow soon.
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- .ci/run-build-and-test.sh
+++ .ci/run-build-and-test.sh
@@ -13,8 +13,24 @@ case "${CI_JOB_NAME}" in
 	*arch*)
 		pacman --noconfirm --refresh --sync --sysupgrade clang meson
 		;;
+	*i686*)
+		BHF_CI_MESON_OPTIONS='--cross-file meson.cross.amd64-linux.i686'
+		;;
+	*mips*)
+		BHF_CI_MESON_OPTIONS='--cross-file meson.cross.amd64-linux.mips'
+		export QEMU_LD_PREFIX='/usr/mips-linux-gnu'
+		export QEMU_USER_EMULATION='qemu-mips'
+		;;
 	*mxe*)
 		BHF_CI_MESON_OPTIONS='--cross-file meson.cross.amd64-linux.win32'
+		;;
+	*riscv64*)
+		BHF_CI_MESON_OPTIONS='--cross-file meson.cross.amd64-linux.riscv64'
+		export QEMU_LD_PREFIX='/usr/riscv64-linux-gnu'
+		export QEMU_USER_EMULATION='qemu-riscv64'
+		;;
+	*tclur*):
+		BHF_CI_MESON_OPTIONS='--native-file meson.native.tclur'
 		;;
 	*)
 		;;
@@ -24,3 +40,13 @@ meson setup build ${BHF_CI_MESON_OPTIONS-}
 ninja -C build
 meson setup example/build example ${BHF_CI_MESON_OPTIONS-}
 ninja -C example/build
+
+# If the job name contains 'test' we run tests, too.
+case "${CI_JOB_NAME}" in
+	*test*)
+		./tools/90_run_tests.sh
+		;;
+	*)
+		printf 'WARNING skipping tests for "%s".\n' "${CI_JOB_NAME}"
+		;;
+esac
